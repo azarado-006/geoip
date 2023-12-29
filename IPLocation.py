8063f0194c526fccd63c192b42299ea700646ea7
AZARADO <145587412+azarado-006@users.noreply.github.com>
2023-12-29 13:17:04 +0530

    Update IPLocation.py

--- IPLocation.py
+++ IPLocation.py
@@ -7,10 +7,10 @@ ylw = "\033[93m"
 
 banner = f"""
 {lblue}  ▀▄  ▄▀
-{lblue} ▄▄▄██▄▄▄▄▄      {red}# {grn}IP Location Finder
-{lblue} █      █▀█      {red}# {grn}Coded By B4rC0d
-{lblue} █      █▀█      {red}# {grn}GiHub ID {red}::: {ylw}BlackFoxTM
-{lblue} █▄▄▄▄▄▄███      {red}# {ylw}Version {red}1.1
+{lblue} ▄▄▄██▄▄▄▄▄▄▄▄      {red}# {grn}IP Location Finder
+{lblue} █ 𓆩₲ɆꝊĪꝐ𓆪 █▀█      {red}# {grn}Coded By ZERO PERCENT
+{lblue} █         █▀█      {red}# {grn}GiHub ID azarado-006
+{lblue} █▄▄▄▄▄▄▄▄▄███      {red}# {grn}Version {ylw}8.8.8.8
 """
 help = f"""
     {red}Usage {ylw}: {grn}python IPLocation.py [OPTION] ...
@@ -31,20 +31,20 @@ def Location(IP):
         print(banner)
         result = get(f'http://ip-api.com/json/{IP}').json()
         print(f"""  
-    {grn}[{red}+{grn}] {lblue}COUNTRY {red}::: {ylw}{result['country']}
-    {grn}[{red}+{grn}] {lblue}COUNTRY CODE {red}::: {ylw}{result['countryCode']}
-    {grn}[{red}+{grn}] {lblue}REGION {red}::: {ylw}{result['region']}
-    {grn}[{red}+{grn}] {lblue}REGION NAME {red}::: {ylw}{result['regionName']}
-    {grn}[{red}+{grn}] {lblue}CITY {red}::: {ylw}{result['city']}
-    {grn}[{red}+{grn}] {lblue}ZIP {red}::: {ylw}{result['zip']}
-    {grn}[{red}+{grn}] {lblue}LAT {red}::: {ylw}{result['lat']}
-    {grn}[{red}+{grn}] {lblue}LON {red}::: {ylw}{result['lon']}
-    {grn}[{red}+{grn}] {lblue}TIME ZONE {red}::: {ylw}{result['timezone']}
-    {grn}[{red}+{grn}] {lblue}ISP {red}::: {ylw}{result['isp']}
-    {grn}[{red}+{grn}] {lblue}ORG {red}::: {ylw}{result['org']}
-    {grn}[{red}+{grn}] {lblue}AS {red}::: {ylw}{result['as']}
-    {grn}[{red}+{grn}] {lblue}IP {red}::: {ylw}{result['query']}
-    {grn}[{red}+{grn}] {lblue}GOOGLE MAP {red}::: {ylw}https://www.google.com/maps/@{result['lat']},{result['lon']},13z
+    {grn}[{red}+{grn}] {lblue}COUNTRY {red}-> {ylw}{result['country']}
+    {grn}[{red}+{grn}] {lblue}COUNTRY CODE {red}-> {ylw}{result['countryCode']}
+    {grn}[{red}+{grn}] {lblue}REGION {red}-> {ylw}{result['region']}
+    {grn}[{red}+{grn}] {lblue}REGION NAME {red}-> {ylw}{result['regionName']}
+    {grn}[{red}+{grn}] {lblue}CITY {red}-> {ylw}{result['city']}
+    {grn}[{red}+{grn}] {lblue}ZIP {red}-> {ylw}{result['zip']}
+    {grn}[{red}+{grn}] {lblue}LAT {red}-> {ylw}{result['lat']}
+    {grn}[{red}+{grn}] {lblue}LON {red}-> {ylw}{result['lon']}
+    {grn}[{red}+{grn}] {lblue}TIME ZONE {red}-> {ylw}{result['timezone']}
+    {grn}[{red}+{grn}] {lblue}ISP {red}-> {ylw}{result['isp']}
+    {grn}[{red}+{grn}] {lblue}ORG {red}-> {ylw}{result['org']}
+    {grn}[{red}+{grn}] {lblue}AS {red}-> {ylw}{result['as']}
+    {grn}[{red}+{grn}] {lblue}IP {red}-> {ylw}{result['query']}
+    {grn}[{red}+{grn}] {lblue}GOOGLE MAP {red}-> {ylw}https://www.google.com/maps/@{result['lat']},{result['lon']},13z
 """)
     except:
         print(f" {red}[{ylw}!{red}] Error {ylw}!!!")
@@ -62,12 +62,12 @@ elif(len(argv) > 1):
             except:
                 print(f"    {red}[{ylw}!{red}] {lblue}Enter the correct IP address ")
         else:
-            print(f"    {red}[{ylw}!{red}] {lblue}Enter the arguments correctly")
+            print(f"    {red}[{ylw}!{red}] {lblue}Enter the arguments in order.")
     elif argv[1] == "--help" or argv[1] == "-h":
             if (len(argv) <= 2):
                 print(banner)
                 print(help)  
             else:
-                print(f"    {red}[{ylw}!{red}] {lblue}Enter the arguments correctly")
+                print(f"    {red}[{ylw}!{red}] {lblue}Enter the correctly")
     else:
         print(f"    {red}[{ylw}!{red}] {lblue}This is not an argument, get help from a guide")
